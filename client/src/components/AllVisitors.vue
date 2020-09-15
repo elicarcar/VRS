be4254e2cdfd51cbb6cfa14ec381e480edf09226
elicarcar <elisabetta.carcar@gmail.com>
2020-09-15 16:28:55 +0200

    display all visitors

---
+++ client/src/components/AllVisitors.vue
@@ -0,0 +1,43 @@
+<template>
+  <table class="visitor-table" v-show="visitors.length">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Visitor</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Visitor</th>
+        <th>Email</th>
+      </tr>
+    </tfoot>
+    <tbody>
+      <tr v-for="visitor in visitors" :key="visitor.id">
+        <td class="status">
+          <span
+            :class="[visitor.is_logged ? 'logged-in' : 'logged-out']"
+          ></span>
+        </td>
+        <td>
+          <router-link :to="`/visitor/${visitor.id}`"
+            >{{ visitor.first_name + visitor.last_name }}
+          </router-link>
+        </td>
+        <td>{{ visitor.email }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'AllVisitors',
+
+  props: {
+    visitors: Array,
+  },
+}
+</script>
