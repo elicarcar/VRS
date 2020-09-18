16bc75958ef96684b7e7aa21d11b839f33066334
elicarcar <elisabetta.carcar@gmail.com>
2020-09-18 14:08:48 +0200

    build basic auth

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,7 +15,7 @@
 }
 
 #nav {
-  padding: 30px;
+  padding: 15px;
 
   a {
     font-weight: bold;
---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="panel">
+    <div class="form-container">
+      <h2>Please login to your account</h2>
+
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            v-model="formData.email"
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" class="form-text text-muted"
+            >We'll never share your email with anyone else.</small
+          >
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input
+            v-model="formData.password"
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div class="form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1"
+            >Check me out</label
+          >
+        </div>
+        <button type="submit" class="btn btn-primary" @click.prevent="login">
+          Login
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+
+  computed: {
+    ...mapState(['alerts']),
+  },
+
+  methods: {
+    ...mapActions(['alert', 'login']),
+    async login() {
+      try {
+        const token = Buffer.from(
+          `${this.formData.email}:${this.formData.password}`,
+          'utf8'
+        ).toString('base64')
+      } catch (error) {
+        console.log(error)
+        this.alert(error.message, 'warning')
+      }
+    },
+  },
+}
+</script>
