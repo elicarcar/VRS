6581a1519ff1c5b19de1c3c395e6d0d86270847a
elicarcar <elisabetta.carcar@gmail.com>
2020-10-19 15:47:34 +0200

    commit before checkout

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,4 +1,4 @@
 export const base_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://thawing-beach-80585.herokuapp.com'
+    ? 'https://thawing-beach-80585.herokuapp.com/'
     : 'http://localhost:5000'
