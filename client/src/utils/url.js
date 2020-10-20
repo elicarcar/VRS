f9315c075676e60fe738fde152d89c1d5b495c10
elicarcar <elisabetta.carcar@gmail.com>
2020-10-20 09:35:33 +0200

    deploy

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,4 +1,4 @@
 export const base_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://thawing-beach-80585.herokuapp.com/'
+    ? 'https://lit-harbor-25561.herokuapp.com/ '
     : 'http://localhost:5000'
